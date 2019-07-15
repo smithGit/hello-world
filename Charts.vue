06d12260f169da71fced67a475bfc1a32b5bf0d5
smithGit <smsmith508@aol.com>
2019-07-14 20:41:49 -0400

    adding charts.vue

---
+++ Charts.vue
@@ -0,0 +1,23 @@
+<!-- d3 charts
+  One source of guidance for d3 on Vue: (assumes chart loaded by install)
+  https://levelup.gitconnected.com/d3-js-and-vue-js-7a6a721eb79f
+  
+  
+  
+  -->
+<template>
+        <q-page padding>
+        <p>Charts page</p>
+    </q-page>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
+
